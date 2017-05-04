5f3b62c37af37161baad154d3927bc07fb932e46
joe werderber and leah sherrell <joe@>
2017-05-04 14:33:30 -0700

    Add functionality to convert numbers 1-8 into Roman Numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,41 @@
 // back-end logic
+// var romans =  ["L", "LX", "X", "IX", "V", "IV", "I"];
+// var numbers = [50,   40,  10,    9,   5,   4,    1];
 
+// var ones = function(num) {
+//
+//   if (num <= 3) {
+//     for (i = 0; i < num; i++) {
+//     output += "I";
+//   }
+// }
 
 var romanize = function(input) {
-  if (typeof input === 'number' && input > 0) {
-    return true;
-  } else {
-    return false;
-  }
+  var output = "";
+  var num = parseInt(input);
+
+  if (num <= 3) {
+    for (i = 0; i < num; i++) {
+      output += "I";
+    }
+  } else if (num === 4) {
+      output = "IV";
+  } else if (num > 4 && num < 9) {
+      output += "V";
+      for (i = 0; i < num - 5; i++) {
+        output += "I";
+      }
+    }
+    return output;
 };
+
 // front-end
 
 $(function() {
 
   $("#form").submit(function(event) {
       event.preventDefault();
-      var userInput = parseInt($("input#number").val());
+      var userInput = $("input#number").val();
       var result = romanize(userInput);
 
       $(".result").text(result);
